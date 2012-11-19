15c772259b1839142c8ce35f01ab439159bd62e7
Sergey G. Grekhov <sgrekhov@unipro.ru>
2012-11-19 15:42:10 +0700

    Copyright updated according w3c requirements

--- test/05_05_Event_Dispatch/A_05_05_01.js
+++ test/05_05_Event_Dispatch/A_05_05_01.js
@@ -1,4 +1,12 @@
-// Copyright 2012 Google Inc. All Rights Reserved.
+/*
+Distributed under both the W3C Test Suite License [1] and the W3C
+3-clause BSD License [2]. To contribute to a W3C Test Suite, see the
+policies and contribution forms [3].
+
+[1] http://www.w3.org/Consortium/Legal/2008/04-testsuite-license
+[2] http://www.w3.org/Consortium/Legal/2008/03-bsd-license
+[3] http://www.w3.org/2004/10/27-testcases
+*/
 
 var A_05_05_01 = {
     name:'A_05_05_01',
@@ -11,9 +19,6 @@ var A_05_05_01 = {
 		'on which event listeners are invoked]]'
 };
 
-//blur and focus events are not bubbling. So this test tests only DOMFocusIn and DOMFocusOut
-//which do bubble
-
 //test DOMFocusOut event
 var A_05_05_01_T01 = async_test('A_05_05_01_T01', PROPS(A_05_05_01, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
@@ -21,7 +26,7 @@ var A_05_05_01_T01 = async_test('A_05_05_01_T01', PROPS(A_05_05_01, {
 }));
 
 A_05_05_01_T01.step(unit(function (ctx) {	
-	ctx.debug = true;
+	
     var d = newRenderedHTMLDocument(ctx);
     
     d.body.innerHTML = '' +
