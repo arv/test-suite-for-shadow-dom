8cdc0ce50041b3b4508c8f51edd76640297b147a
mfursov@unipro.ru <mfursov@unipro.ru>
2012-11-22 15:58:47 +0700

    adding test.html to autogeneration

--- tools/spec_version_tracker/svt_coverage.php
+++ tools/spec_version_tracker/svt_coverage.php
@@ -6,31 +6,35 @@
 require_once "svt_common.php";
 
 try {
-    $content = fetch_spec_content();
+    $content_all_tests_js = fetch_spec_content();
 
-    $content = str_replace("a href=\"http",
-        "a target='_blank' href=\"http", $content);
+    $content_all_tests_js = str_replace("a href=\"http",
+        "a target='_blank' href=\"http", $content_all_tests_js);
 
-    $content = str_replace("\"../../",
-        "\"$spec_url_dir/../../", $content);
+    $content_all_tests_js = str_replace("\"../../",
+        "\"$spec_url_dir/../../", $content_all_tests_js);
 
     $spec_dir = safe_path(getcwd() . "/../../test/spec");
     $spec_file_name = "$spec_dir/shadow_dom_spec.html";
-//    save_file_and_commit($spec_file_name, $content, $spec_dir, "automatic update of shadow dom spec used by  coverage");
+    save_file_and_commit($spec_file_name, $content_all_tests_js, $spec_dir, "automatic update of shadow dom spec used by  coverage");
 
 
     $tests_dir = safe_path(getcwd() . "/../../test");
-    $tests_file_name = "$tests_dir/all_tests.js";
+    $all_tests_js_file_name = "$tests_dir/all_tests.js";
+    $tests_html_file_name = "$tests_dir/test.html";
     $test_files = get_files_from_dir($tests_dir, "js");
-    $content = "";
+    $content_all_tests_js = "";
+    $content_tests_html = "";
     $trim_size = strlen($tests_dir) + 1;
     foreach ($test_files as $test) {
         $name = substr($test, $trim_size);
-        if (strpos($name, "A_0") > 0) {
-            $content = "$content\ninclude(\"$name\");";
+        if (strpos($name, "A_") > 0) {
+            $content_all_tests_js = "$content_all_tests_js\ninclude(\"$name\");";
+            $content_tests_html = $content_tests_html . "\n<script src=\"$name\"></script>";
         }
     }
-    save_file_and_commit($tests_file_name, $content, $tests_dir + "/..", "\"auto update of 'all_tests.js' and spec file used by coverage\"");
+    save_file_and_commit($all_tests_js_file_name, $content_all_tests_js, $tests_dir + "/..", "\"auto update of '$all_tests_js_file_name'\"");
+    save_file_and_commit($tests_html_file_name, $content_tests_html, $tests_dir + "/..", "\"auto update of '$tests_html_file_name'\"");
 } catch (Exception $e) {
     halt("Failed with exception!:" . $e->getMessage());
 }
