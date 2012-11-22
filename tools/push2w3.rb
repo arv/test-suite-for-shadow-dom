cc746ff34cd4537409a5893bbac8ec14cd7a1c28
Hajime Morita <morrita@dhcp-172-30-89-1.tok.corp.google.com>
2012-11-22 09:21:25 +0900

    Added a tweak to use spec_coverage.html as a teset runner on the w3c repo.

--- tools/push2w3.rb
+++ tools/push2w3.rb
@@ -2,7 +2,9 @@
 
 require 'fileutils'
 
-PREVENT_FROM_REMOVAL = ["/LICENSE"]
+DONT_REMOVE = ["/LICENSE", "/index.html"]
+DONT_COPY = ["/spec_coverage.html", "/test.html"]
+
 SRC_DIR = File.expand_path(File.join(File.dirname(__FILE__), "..", "test"))
 DST_DIR = File.expand_path(File.join(ARGV[0], *"ShadowDOM/tests/submissions/Google".split("/")))
 
@@ -15,8 +17,10 @@ def copy_files(src, dst)
   src_files.each do |s|
     relative = s.sub(SRC_DIR, "")
     d = File.join(dst, relative)
-    copy_file(s, d) if File.file?(s)
+    copy_file(s, d) if File.file?(s) and not DONT_COPY.include?(s.sub(src, ""))
   end
+
+  copy_file(File.join(src, "/spec_coverage.html"), File.join(dst, "/index.html"))
 end
 
 def copy_file(src, dst)
@@ -24,10 +28,11 @@ def copy_file(src, dst)
   ddir = File.dirname(dst)
   FileUtils.mkdir_p(ddir) unless File.exist?(ddir)
   FileUtils.cp(src, dst)
+  p "#{src} #{dst}"
 end
 
 def remove_missings(src, dst)
-  dont_remove = list_files(src).map { |i| i.sub(src, "") } + PREVENT_FROM_REMOVAL
+  dont_remove = list_files(src).map { |i| i.sub(src, "") } + DONT_REMOVE
   list_files(dst).each do |d|
     d_relative = d.sub(dst, "")
     unless dont_remove.include?(d_relative)
