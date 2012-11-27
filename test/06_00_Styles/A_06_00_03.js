e9b909799bacea45541e1d09a364b03a9d6caf9d
Sergey G. Grekhov <sgrekhov@unipro.ru>
2012-11-27 20:21:56 +0700

    Test rewritten to not use external iframe

--- test/06_00_Styles/A_06_00_03.js
+++ test/06_00_Styles/A_06_00_03.js
@@ -18,74 +18,61 @@ var A_06_00_03 = {
     	'named shadow root style sheets'
 };
 
+
 //TODO Now this tests produces an error because styleSheets property 
-// is not implemented. See https://bugs.webkit.org/show_bug.cgi?id=103393
-var A_06_00_03_T01 = async_test('A_06_00_03_T01', PROPS(A_06_00_03, {
+//is not implemented. See https://bugs.webkit.org/show_bug.cgi?id=103393
+test(unit(function (ctx) {	
+    var d = newRenderedHTMLDocument(ctx);
+    var host = d.createElement('div');
+    d.body.appendChild(host);
+
+	//Shadow root to play with
+	var s = new SR(host);
+	
+	assert_equals(s.styleSheets.length, 0, 'There should be no style sheets');
+}), 'A_06_00_03_T01', PROPS(A_06_00_03, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
     reviewer:''
-}));
-
-A_06_00_03_T01.step(unit(function (ctx) {	
-	
-	var iframe = newIFrame(ctx, 'resources/bobs_page.html');
-	
-	iframe.onload = A_06_00_03_T01.step_func(function() {
-		var d = iframe.contentWindow.document;
-
-	    var ul = d.querySelector('ul.stories');
-	    var s = new SR(ul);
-
-		assert_equals(s.styleSheets.length, 0, 'There should be no style sheets');
-		A_06_00_03_T01.done();
-    });		
 }));
 
 
 //TODO Now this tests produces an error because styleSheets property 
 //is not implemented. See https://bugs.webkit.org/show_bug.cgi?id=103393
-var A_06_00_03_T02 = async_test('A_06_00_03_T02', PROPS(A_06_00_03, {
+test(unit(function (ctx) {	
+    var d = newRenderedHTMLDocument(ctx);
+    var host = d.createElement('div');
+    host.setAttribute('style', 'width:100px');
+    d.body.appendChild(host);
+
+	//Shadow root to play with
+	var s = new SR(host);
+	
+	assert_equals(s.styleSheets.length, 0, 'There should be no style sheets');
+}), 'A_06_00_03_T02', PROPS(A_06_00_03, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
     reviewer:''
 }));
 
-A_06_00_03_T02.step(unit(function (ctx) {	
-	
-	var iframe = newIFrame(ctx, 'resources/bobs_page.html');
-	
-	iframe.onload = A_06_00_03_T02.step_func(function() {
-		var d = iframe.contentWindow.document;
-
-	    var ul = d.querySelector('ul.stories');
-	    ul.setAttribute('style', 'width:100px');
-	    var s = new SR(ul);
-
-		assert_equals(s.styleSheets.length, 0, 'There should be no style sheets');
-		A_06_00_03_T02.done();
-    });		
-}));
 
 
 //TODO Now this tests produces an error because addStyleSheet method 
 //is not implemented. See https://bugs.webkit.org/show_bug.cgi?id=103395
-var A_06_00_03_T03 = async_test('A_06_00_03_T03', PROPS(A_06_00_03, {
+test(unit(function (ctx) {	
+    var d = newRenderedHTMLDocument(ctx);
+    var host = d.createElement('div');
+    host.setAttribute('style', 'width:100px');
+    d.body.appendChild(host);
+
+	//Shadow root to play with
+	var s = new SR(host);
+	
+    // create StyleSheet
+    var style = d.createElement('style');
+    s.addStyleSheet(style);
+	
+    assert_equals(s.styleSheets.length, 1, 'Style sheet is not accessible via styleSheets');
+}), 'A_06_00_03_T03', PROPS(A_06_00_03, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
     reviewer:''
 }));
 
-A_06_00_03_T03.step(unit(function (ctx) {	
-	
-	var iframe = newIFrame(ctx, 'resources/bobs_page.html');
-	
-	iframe.onload = A_06_00_03_T03.step_func(function() {
-		var d = iframe.contentWindow.document;
-
-	    var ul = d.querySelector('ul.stories');
-	    var s = new SR(ul);
-	    // create StyleSheet
-	    var style = d.createElement('style');
-	    s.addStyleSheet(style);
-
-		assert_equals(s.styleSheets.length, 1, 'Style sheet is not accessible via styleSheets');
-		A_06_00_03_T03.done();
-    });		
-}));
