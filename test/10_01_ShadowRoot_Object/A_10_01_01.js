f9c432a6acaf0558a1630134de392a7bc0503ab3
Sergey G. Grekhov <sgrekhov@unipro.ru>
2012-12-12 17:05:00 +0700

    Tests for ShadowRoot object methods

---
+++ test/10_01_ShadowRoot_Object/A_10_01_01.js
@@ -0,0 +1,60 @@
+/*
+Distributed under both the W3C Test Suite License [1] and the W3C
+3-clause BSD License [2]. To contribute to a W3C Test Suite, see the
+policies and contribution forms [3].
+
+[1] http://www.w3.org/Consortium/Legal/2008/04-testsuite-license
+[2] http://www.w3.org/Consortium/Legal/2008/03-bsd-license
+[3] http://www.w3.org/2004/10/27-testcases
+*/
+
+var A_10_01_01 = {
+    name:'A_10_01_01',
+    assert:'ShadowRoot Object: ' +
+    	'HTMLElement getElementById(DOMString elementId) method',
+    link:'https://dvcs.w3.org/hg/webcomponents/raw-file/tip/spec/shadow/index.html#shadow-root-object',
+    highlight: '[[The ShadowRoot object represents the shadow root.]]' +
+    	'[\\s\\S]*[[HTMLElement getElementById(DOMString elementId);]]'
+};
+
+test(function () {
+	
+	var d = newHTMLDocument();
+	
+    var el = d.createElement('div');
+    d.body.appendChild(el);
+    
+    var s = new SR(el);
+    
+    var child = d.createElement('span');
+    child.setAttribute('id', 'span_id');
+    s.appendChild(child);
+
+    assert_true(s.getElementById('span_id') != null, 'Point 1: ShadowRoot getElementById() ' +
+    		'method should return child element');
+    assert_equals(s.getElementById('span_id').getAttribute('id'), 'span_id', 'Point 2: ' +
+    		'ShadowRoot getElementById() method should return child element');
+        
+}, 'A_10_01_01_T01', PROPS(A_10_01_01, {
+	author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
+	reviewer:''
+}));
+
+
+
+test(function () {
+	
+	var d = newHTMLDocument();
+	
+    var el = d.createElement('div');
+    d.body.appendChild(el);
+    
+    var s = new SR(el);
+    
+    assert_true(s.getElementById('span_id') == null, ' ShadowRoot getElementById() ' +
+    		'method should return null if matching element not found');
+        
+}, 'A_10_01_01_T02', PROPS(A_10_01_01, {
+	author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
+	reviewer:''
+}));
