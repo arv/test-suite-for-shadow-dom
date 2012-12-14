bf8983119f1045bb4f3478ebd3f5dedd676f8177
Sergey G. Grekhov <sgrekhov@unipro.ru>
2012-12-14 18:25:45 +0700

    Extentions to the Element interface

---
+++ test/10_02_Extensions_to_Element_Interface/A_10_02_01.js
@@ -0,0 +1,55 @@
+/*
+Distributed under both the W3C Test Suite License [1] and the W3C
+3-clause BSD License [2]. To contribute to a W3C Test Suite, see the
+policies and contribution forms [3].
+
+[1] http://www.w3.org/Consortium/Legal/2008/04-testsuite-license
+[2] http://www.w3.org/Consortium/Legal/2008/03-bsd-license
+[3] http://www.w3.org/2004/10/27-testcases
+*/
+
+var A_10_02_01 = {
+    name:'A_10_02_01',
+    assert:'Extensions to Element Interface: ' +
+    	'pseudo of type DOMString attribute. Test getter when there is a custom pseudo-element ' +
+    	'associated with this element',
+    link:'https://dvcs.w3.org/hg/webcomponents/raw-file/tip/spec/shadow/index.html#shadow-aware-attributes',
+    highlight: '[[pseudo of type DOMString]]' +
+    	'[\\s\\S]*[[On getting, the attribute must return the current custom pseudo-element value]]'
+};
+
+test(function () {
+	
+	var d = newHTMLDocument();
+	    
+    var widget = d.createElement('div');
+    d.body.appendChild(widget);
+    
+    var s = new SR(widget);
+    
+    var thumb = d.createElement('span');
+    thumb.innerHTML = 'This is a pseudo-element';
+    //FIXME test works if prefixed version of API used. 
+    //In other words works if webkitPseudo property is used
+    thumb.webkitPseudo = 'x-thumb';
+    //thumb.pseudo = 'x-thumb';
+    s.appendChild(thumb);
+    
+    var style = d.createElement('style');
+    style.innerHTML = '' + 
+    	'div::x-thumb {' +
+    	'font-size: 30px;' +
+    	'}';
+    d.body.appendChild(style);
+    
+    //FIXME replace webkitPseudo by pseudo
+    assert_true(thumb.webkitPseudo != null, 'attribute \'pseudo\' must not be null');
+    
+    assert_equals(thumb.webkitPseudo, 'x-thumb', 'attribute \'pseudo\' must return ' +
+    		'the current custom pseudo-element value')
+        
+}, 'A_10_02_01_T01', PROPS(A_10_02_01, {
+	author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
+	reviewer:''
+}));
+
