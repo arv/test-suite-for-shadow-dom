fa2c9933e825de5fe4516488b43b9dd1c270ba96
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2012-12-27 17:26:45 +0700

    test review

--- test/10_01_01_ShadowRoot_Attributes/A_10_01_01_02_01.js
+++ test/10_01_01_ShadowRoot_Attributes/A_10_01_01_02_01.js
@@ -18,10 +18,10 @@ var A_10_01_01_02_01 = {
 
 //test default value
 test(unit(function (ctx) {
-	
+
 	var d = newRenderedHTMLDocument(ctx);
-    
-    d.body.innerHTML = 
+
+    d.body.innerHTML =
     	'<ul id="shHost">' +
     		'<li id="li1" class="shadow">1</li>' +
     		'<li id="li2" class="shadow2">2</li>' +
@@ -31,66 +31,69 @@ test(unit(function (ctx) {
     		'<li id="li6" class="shadow2">6</li>' +
     	'</ul>';
 
-    
+
 	var defHeight1 = d.querySelector('#li1').offsetHeight;
 	var defHeight2 = d.querySelector('#li2').offsetHeight;
 	var defHeight3 = d.querySelector('#li3').offsetHeight;
 	var defHeight4 = d.querySelector('#li4').offsetHeight;
 	var defHeight5 = d.querySelector('#li5').offsetHeight;
 	var defHeight6 = d.querySelector('#li6').offsetHeight;
-	
+
 	assert_true(defHeight1 > 0, 'Point 1: Element height should be greater than zero');
 	assert_true(defHeight2 > 0, 'Point 2: Element height should be greater than zero');
 	assert_true(defHeight3 > 0, 'Point 3: Element height should be greater than zero');
 	assert_true(defHeight4 > 0, 'Point 4: Element height should be greater than zero');
 	assert_true(defHeight5 > 0, 'Point 5: Element height should be greater than zero');
 	assert_true(defHeight6 > 0, 'Point 6: Element height should be greater than zero');
-    
+
     d.body.setAttribute('style', 'font-size: 30px');
-    
+
 	var height1 = d.querySelector('#li1').offsetHeight;
 	var height2 = d.querySelector('#li2').offsetHeight;
 	var height3 = d.querySelector('#li3').offsetHeight;
 	var height4 = d.querySelector('#li4').offsetHeight;
 	var height5 = d.querySelector('#li5').offsetHeight;
 	var height6 = d.querySelector('#li6').offsetHeight;
-    
-    
+
+
 	assert_true(height1 > defHeight1, 'Point 11: Element height should be changed');
 	assert_true(height2 > defHeight2, 'Point 12: Element height should be changed');
 	assert_true(height3 > defHeight3, 'Point 13: Element height should be changed');
 	assert_true(height4 > defHeight4, 'Point 14: Element height should be changed');
 	assert_true(height5 > defHeight5, 'Point 15: Element height should be changed');
 	assert_true(height6 > defHeight6, 'Point 16: Element height should be changed');
-    
+
 	//Shadow root to play with
 	var host = d.querySelector('#shHost');
-    var s = createSR(host);    
+    var s = createSR(host);
 
-	var div = d.createElement('div');	
-	div.innerHTML ='<ul><content select=".shadow"></content></ul>'; 
+    assert_false(s.resetStyleInheritance, 'Default value for ShadowRoot Object attribute resetStyleInheritance' +
+    		' is expected to be false');
+
+	var div = d.createElement('div');
+	div.innerHTML ='<ul><content select=".shadow"></content></ul>';
 	s.appendChild(div);
-	
+
 	assert_equals(d.querySelector('#li1').offsetHeight, height1, 'Point 21: Element height should not be changed');
 	assert_equals(d.querySelector('#li3').offsetHeight, height3, 'Point 22: Element height should not be changed');
 	assert_equals(d.querySelector('#li5').offsetHeight, height5, 'Point 23: Element height should not be changed');
-	
+
 	assert_equals(d.querySelector('#li2').offsetHeight, 0, 'Point 24: Element shouldn\'t be rendered');
 	assert_equals(d.querySelector('#li4').offsetHeight, 0, 'Point 25: Element shouldn\'t be rendered');
 	assert_equals(d.querySelector('#li6').offsetHeight, 0, 'Point 26: Element shouldn\'t be rendered');
-	
+
 }), 'A_10_01_01_02_01_T01', PROPS(A_10_01_01_02_01, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
-    reviewer:''
+    reviewer:'Aleksei Yu. Semenov <a.semenov@unipro.ru>'
 }));
 
 
 //test implicit value
 test(unit(function (ctx) {
-	
+
 	var d = newRenderedHTMLDocument(ctx);
-    
-    d.body.innerHTML = 
+
+    d.body.innerHTML =
     	'<ul id="shHost">' +
     		'<li id="li1" class="shadow">1</li>' +
     		'<li id="li2" class="shadow2">2</li>' +
@@ -99,57 +102,57 @@ test(unit(function (ctx) {
     		'<li id="li5" class="shadow">5</li>' +
     		'<li id="li6" class="shadow2">6</li>' +
     	'</ul>';
-    
+
 	var defHeight1 = d.querySelector('#li1').offsetHeight;
 	var defHeight2 = d.querySelector('#li2').offsetHeight;
 	var defHeight3 = d.querySelector('#li3').offsetHeight;
 	var defHeight4 = d.querySelector('#li4').offsetHeight;
 	var defHeight5 = d.querySelector('#li5').offsetHeight;
 	var defHeight6 = d.querySelector('#li6').offsetHeight;
-	
+
 	assert_true(defHeight1 > 0, 'Point 1: Element height should be greater than zero');
 	assert_true(defHeight2 > 0, 'Point 2: Element height should be greater than zero');
 	assert_true(defHeight3 > 0, 'Point 3: Element height should be greater than zero');
 	assert_true(defHeight4 > 0, 'Point 4: Element height should be greater than zero');
 	assert_true(defHeight5 > 0, 'Point 5: Element height should be greater than zero');
 	assert_true(defHeight6 > 0, 'Point 6: Element height should be greater than zero');
-    
+
     d.body.setAttribute('style', 'font-size: 30px');
-    
+
 	var height1 = d.querySelector('#li1').offsetHeight;
 	var height2 = d.querySelector('#li2').offsetHeight;
 	var height3 = d.querySelector('#li3').offsetHeight;
 	var height4 = d.querySelector('#li4').offsetHeight;
 	var height5 = d.querySelector('#li5').offsetHeight;
 	var height6 = d.querySelector('#li6').offsetHeight;
-    
-    
+
+
 	assert_true(height1 > defHeight1, 'Point 11: Element height should be changed');
 	assert_true(height2 > defHeight2, 'Point 12: Element height should be changed');
 	assert_true(height3 > defHeight3, 'Point 13: Element height should be changed');
 	assert_true(height4 > defHeight4, 'Point 14: Element height should be changed');
 	assert_true(height5 > defHeight5, 'Point 15: Element height should be changed');
 	assert_true(height6 > defHeight6, 'Point 16: Element height should be changed');
-    
+
 	//Shadow root to play with
 	var host = d.querySelector('#shHost');
     var s = createSR(host);
     s.resetStyleInheritance = false;
 
-	var div = d.createElement('div');	
-	div.innerHTML ='<ul><content select=".shadow"></content></ul>'; 
+	var div = d.createElement('div');
+	div.innerHTML ='<ul><content select=".shadow"></content></ul>';
 	s.appendChild(div);
-	
+
 	assert_equals(d.querySelector('#li1').offsetHeight, height1, 'Point 21: Element height should not be changed');
 	assert_equals(d.querySelector('#li3').offsetHeight, height3, 'Point 22: Element height should not be changed');
 	assert_equals(d.querySelector('#li5').offsetHeight, height5, 'Point 23: Element height should not be changed');
-	
+
 	assert_equals(d.querySelector('#li2').offsetHeight, 0, 'Point 24: Element shouldn\'t be rendered');
 	assert_equals(d.querySelector('#li4').offsetHeight, 0, 'Point 25: Element shouldn\'t be rendered');
 	assert_equals(d.querySelector('#li6').offsetHeight, 0, 'Point 26: Element shouldn\'t be rendered');
-	
+
 }), 'A_10_01_01_02_01_T02', PROPS(A_10_01_01_02_01, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
-    reviewer:''
+    reviewer:'Aleksei Yu. Semenov <a.semenov@unipro.ru>'
 }));
 
