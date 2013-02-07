f00efcd4bb8a7fabdd1bd8f885a1c9efbe400465
Aleksei Yu. Semenov <a.semenov@unipro.ru>
2013-02-07 17:34:02 +0700

    test review and fixes

--- test/05_02_Retargeting_relatedTarget/A_05_02_01.js
+++ test/05_02_Retargeting_relatedTarget/A_05_02_01.js
@@ -23,46 +23,46 @@ var A_05_02_01 = {
 
 var A_05_02_01_T01 = async_test('A_05_02_01_T1', PROPS(A_05_02_01, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
-    reviewer:''
+    reviewer:'Aleksei Yu. Semenov <a.semenov@unipro.ru>'
 }));
 
-A_05_02_01_T01.step(unit(function (ctx) {	
-	
+A_05_02_01_T01.step(unit(function (ctx) {
+
     var d = newRenderedHTMLDocument(ctx);
-    
+
     var host = d.createElement('div');
     host.setAttribute('style', 'height:100%; width:100%');
     host.setAttribute('id', 'host');
     d.body.appendChild(host);
-    
+
     //Shadow root to play with
     var s = createSR(host);
-    
+
     var div1 = d.createElement('div');
     div1.setAttribute('style', 'height:40px; width:100%');
     div1.setAttribute('id', 'div1');
     s.appendChild(div1);
-    
+
     var div2 = d.createElement('div');
     div2.setAttribute('style', 'height:40px; width:100%');
     div2.setAttribute('id', 'div2');
     s.appendChild(div2);
-    
+
     s.addEventListener('mouseover', A_05_02_01_T01.step_func(function(event) {
-    	assert_equals(event.relatedTarget.getAttribute('id'), 'div1', 'Wrong relatedTarget');  	
+    	assert_equals(event.relatedTarget.getAttribute('id'), 'div1', 'Wrong relatedTarget');
     }), false);
-    
+
     d.body.addEventListener('mouseover', A_05_02_01_T01.step_func(function(event) {
-    	assert_true(false, 'Event must be stopped at Shadow boundary');  	
+    	assert_true(false, 'Event must be stopped at Shadow boundary');
     }), false);
-    
-    
+
+
     var evt = document.createEvent("MouseEvents");
     evt.initMouseEvent("mouseover", true, false, window,
       0, 10, 10, 10, 10, false, false, false, false, 0, div1);
-    
+
     div2.dispatchEvent(evt);
-    
-    
+
+
     A_05_02_01_T01.done();
 }));
--- test/05_02_Retargeting_relatedTarget/A_05_02_03.js
+++ test/05_02_Retargeting_relatedTarget/A_05_02_03.js
@@ -20,41 +20,48 @@ var A_05_02_03 = {
 
 var A_05_02_03_T01 = async_test('A_05_02_03_T01', PROPS(A_05_02_03, {
     author:'Sergey G. Grekhov <sgrekhov@unipro.ru>',
-    reviewer:''
+    reviewer:'Aleksei Yu. Semenov <a.semenov@unipro.ru>'
 }));
 
-A_05_02_03_T01.step(unit(function (ctx) {	
-	
+A_05_02_03_T01.step(unit(function (ctx) {
+
     var d = newRenderedHTMLDocument(ctx);
-    
+
     var host = d.createElement('div');
     host.setAttribute('style', 'height:50%; width:100%');
     host.setAttribute('id', 'host');
     d.body.appendChild(host);
-    
+
     //Shadow root to play with
     var s = createSR(host);
-    
+
     var div1 = d.createElement('div');
     div1.setAttribute('style', 'height:100%; width:100%');
     div1.setAttribute('id', 'div1');
     s.appendChild(div1);
-    
+
     var div2 = d.createElement('div');
     div2.setAttribute('style', 'height:100%; width:100%');
     div2.setAttribute('id', 'div2');
     s.appendChild(div2);
-    
-    s.addEventListener('mouseover', A_05_02_03_T01.step_func(function(event) {
-    	assert_true(false, 'Event listeners shouldn\'t be invoked if target and relatedTarget are the same');  	
+
+    d.body.addEventListener('mouseover', A_05_02_03_T01.step_func(function(event) {
+    	assert_true(false, 'Event listeners shouldn\'t be invoked if target and relatedTarget are the same');
     }), false);
-    
-    
+
+    host.addEventListener('mouseover', A_05_02_03_T01.step_func(function(event) {
+    	assert_true(false, 'Event listeners shouldn\'t be invoked if target and relatedTarget are the same');
+    }), false);
+
+    s.addEventListener('mouseover', A_05_02_03_T01.step_func(function(event) {
+    	assert_true(true, 'Event listeners should be invoked');
+    }), false);
+
     var evt = document.createEvent("MouseEvents");
     evt.initMouseEvent("mouseover", true, false, window,
-      0, 10, 10, 10, 10, false, false, false, false, 0, div1);
-    
+      0, 10, 10, 10, 10, false, false, false, false, 0, div2);
+
     div1.dispatchEvent(evt);
-    
+
     A_05_02_03_T01.done();
 }));
